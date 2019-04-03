59d25cb2883f14248290f8ccc4b4bb4b2da06cdb
Irving Didier <bider88@gmail.com>
2019-04-02 21:41:29 -0600

    Getting info from post request and through request param

--- routes/router.ts
+++ routes/router.ts
@@ -10,9 +10,28 @@ router.get('/messages', (req: Request, res: Response) => {
 });
 
 router.post('/messages', (req: Request, res: Response) => {
+
+    const { body, from } = req.body;
+
     res.json({
         ok: true,
-        message: 'POST: All Ok!'
+        message: 'POST: All Ok!',
+        body,
+        from
+    })
+});
+
+router.post('/messages/:id', (req: Request, res: Response) => {
+
+    const { body, from } = req.body;
+    const { id } = req.params;
+
+    res.json({
+        ok: true,
+        message: 'POST: All Ok!',
+        body,
+        from,
+        id
     })
 });
 
