2dc47bf23e65673ed5e5fb3ffb0d7679e245a639
Irving Didier <bider88@gmail.com>
2019-04-10 00:21:09 -0500

    Adding configuratons for sockets.io

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,35 @@
 import express from 'express';
 import { SERVER_PORT } from './../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
     
     app: express.Application;
     port: number;
 
+    io: socketIO.Server;
+    httpServer: http.Server;
+
     constructor() {
         this.app = express();
-        this.port = SERVER_PORT
+        this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.ListenSockets();
     }
 
-    start(callback: Function) {
-        this.app.listen( this.port, callback );
+    private ListenSockets() {
+        console.log(`Listening request - sockets`);
+
+        this.io.on('connection', client => {
+            console.log('client connected')
+        });
+    }
+
+    start(callback: any) {
+        this.httpServer.listen( this.port, callback );
     }
 }
