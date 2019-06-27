2ba2b240f262029ed0298c01383844a6806fab16
Irving Didier <bider88@gmail.com>
2019-06-26 21:46:14 -0500

    Adding clasess user and user list

---
+++ classes/user-list.ts
@@ -0,0 +1,47 @@
+import { User } from './user';
+
+export class UserList {
+    private list: User[] = [];
+
+    constructor() {
+
+    }
+
+    addUser(user: User) {
+        this.list.push(user);
+        console.log(this.list);
+        return user;
+    }
+
+    updateName(id: string, name: string) {
+        this.list.forEach( user => {
+            if (user.id === id) {
+                user.name = name;
+            }
+        });
+
+        console.log('Updating user', this.list);
+    }
+
+    getListOfUsers() {
+        return this.list;
+    }
+
+    getUser(id: string) {
+        return this.list.find(user => user.id === id);
+    }
+
+    getUsersByRoom(room: string) {
+        return this.list.filter(user => user.room === room);
+    }
+
+    deleteUser(id: string) {
+        const userTemp: User | undefined = this.getUser(id);
+
+        if (userTemp) {
+            this.list = this.list.filter(user => user.id !== userTemp.id);
+        }
+
+        return userTemp;
+    }
+}
